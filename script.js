c8af263c8a3fdddcf90e976319595c105ea5f91b
Flavian-K <flaviankibe@gmail.com>
2024-05-31 21:59:15 +0300

    added a withdrawing function and added some more logic to depositing function in script.js, added some elements in html

--- script.js
+++ script.js
@@ -13,48 +13,125 @@
 //   } else if (name == "Joyce") {
 //     return "Joyce has been summoned";
 //   } else {
-//     return "Another student has been summoned";
+//     return null;
 //   }
 // }
 
 // console.log(callStudent("Claire"));
-let min = 20;
-let max = 5000;
 
-function getUserInput() {
-  let userInput = document.getElementById("amount").value;
+let userInput = document.getElementById("amount").value;
+let balanceElement = document.getElementById("balance");
 
-  if (userInput < min) {
-    console.log("Deposit amount is too little. It needs to be more than ", min);
-    return;
-  } else if (userInput > max) {
-    console.log("Deposit amount is too much. It needs to be less than ", max);
-    return;
-  }
+const errorElement = document.getElementById("error");
+const successElement = document.getElementById("success");
 
-  console.log("The value of amount is: ", userInput);
-  console.log("The type of amount is: ", typeof userInput);
+const success2Element = document.getElementById("success2");
+const error2Element = document.getElementById("error2");
 
-  let balanceElement = document.getElementById("balance");
+function clearMessages() {
+	successElement.textContent = "";
+	errorElement.textContent = "";
+	success2Element.textContent = "";
+	error2Element.textContent = "";
+}
 
-  let balance = balanceElement.innerText;
+function depositing() {
+	clearMessages();
+	let min = 20;
+	let max = 5000;
+	userInput = document.getElementById("amount").value;
+	console.log(userInput);
+	console.log(typeof userInput);
+	// let errorElement = document.getElementById("error");
+	// let successElement = document.getElementById("success");
+	console.log(errorElement.textContent);
+	if (userInput < min) {
+		successElement.textContent = "";
+		errorElement.textContent =
+			"Deposit amount is too little. It needs to be more than " + min;
+		return;
+	} else if (userInput > max) {
+		successElement.textContent = "";
+		errorElement.textContent =
+			"Deposit amount is too much. It needs to be less than " + max;
+		return;
+	}
 
-  console.log("The type of balance is: ", typeof balance);
+	console.log("The value of amount is: ", userInput);
+	console.log("The type of amount is: ", typeof userInput);
 
-  balance = parseInt(balance);
-  console.log("The type of balance is: ", typeof balance);
+	balanceElement = document.getElementById("balance");
 
-  userInput = parseInt(userInput);
-  console.log("The type of amount is: ", typeof userInput);
+	let balance = balanceElement.innerText;
 
-  console.log("The current balance is: ", balance);
+	console.log("The type of balance is: ", typeof balance);
 
-  let newBalance = balance + userInput;
-  console.log("The new balance is: ", newBalance);
+	balance = parseInt(balance);
+	console.log("The type of balance is: ", typeof balance);
 
-  balanceElement.innerText = newBalance;
+	userInput = parseInt(userInput);
+	console.log("The type of amount is: ", typeof userInput);
+
+	console.log("The current balance is: ", balance);
+
+	let newBalance = balance + userInput;
+	console.log("The new balance is: ", newBalance);
+
+	balanceElement.innerText = newBalance;
+
+	errorElement.textContent = "";
+	successElement.textContent = "deposit was successful";
+	userInput = document.getElementById("amount").value = "";
 }
 
 let submitButton = document.getElementById("deposit-btn");
 
-submitButton.addEventListener("click", getUserInput);
+submitButton.addEventListener("click", depositing);
+
+// let errorParagragh = document.getElementById("error2");
+
+// console.log(errorParagragh);
+
+function withdrawing() {
+	clearMessages();
+	// let success2Element = document.getElementById("success2");
+	// let error2Element = document.getElementById("error2");
+	// success2Element.textContent = "";
+	// error2Element.textContent = "";
+	userInput = document.getElementById("amount").value;
+	balanceElement = document.getElementById("balance");
+	let balance = balanceElement.innerText;
+	console.log("The type of balance is: ", typeof balance);
+
+	balance = parseInt(balance);
+	console.log("The type of balance is: ", typeof balance);
+	let max = balance;
+	let min = 50;
+	if (userInput > balance) {
+		success2Element.textContent = "";
+		error2Element.textContent = "withdrawal amount exceeds balance: " + max;
+		return;
+	} else if (min > userInput) {
+		success2Element.textContent = "";
+		error2Element.textContent =
+			"Withdrawal amount is less than minimum allowed withdrawal: " + min;
+		return;
+	}
+
+	userInput = parseInt(userInput);
+	console.log("The type of amount is: ", typeof userInput);
+
+	console.log("The current balance is: ", balance);
+
+	let newBalance = balance - userInput;
+	console.log("The new balance is: ", newBalance);
+
+	balanceElement.innerText = newBalance;
+
+	error2Element.textContent = "";
+	success2Element.textContent =
+		"Withdrawal is successfull. New balance: " + newBalance;
+	userInput = document.getElementById("amount").value = "";
+}
+let withdrawElement = document.getElementById("withdraw-btn");
+withdrawElement.addEventListener("click", withdrawing);
